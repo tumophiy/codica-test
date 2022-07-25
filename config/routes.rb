9c995decb9a0a91f75e8a6600f61a762399edbcc
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-25 11:52:57 +0300

    add gem administrate and generate admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :doctors
+      resources :doctor_types
+      resources :patients
+      resources :visits
+
+      root to: "users#index"
+    end
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
